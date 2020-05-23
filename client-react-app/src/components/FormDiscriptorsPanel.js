f18fabe5998388292fb6e6011dfc01fce5e3ce07
fariddhessami <fariddhessami@gmail.com>
2020-05-23 18:13:04 +0430

    a new component has been added ... the first

---
+++ client-react-app/src/components/FormDiscriptorsPanel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './App.css';
+
+function FormDiscPanel() {
+  return (
+    <div className="FormDiscPanel">
+      <h3>form discriptors</h3>
+    </div>
+  );
+}
+
+export default FormDiscPanel;
