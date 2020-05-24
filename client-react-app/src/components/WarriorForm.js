ac5c576f70076a6fcb756b57736a07577f6e8385
fariddhessami <fariddhessami@gmail.com>
2020-05-24 21:09:11 +0430

    begining to adding tr s

--- client-react-app/src/components/WarriorForm.js
+++ client-react-app/src/components/WarriorForm.js
@@ -55,6 +55,15 @@ export class WarriorForm extends Component {
                   <th>branch</th>
                 </tr>
               </thead>
+              <tbody>
+                {this.props.warriosArray.map((aWarrior) => (
+                  <tr>Fname</tr>
+                  // <tr>{aWarrior.Fname}</tr>
+                  // <tr>{aWarrior.Lname}</tr>
+                  // <tr>{aWarrior.serviceId}</tr>
+                  // <tr>{aWarrior.branch}</tr>
+                ))}
+              </tbody>
             </table>
           </form>
         </div>
@@ -64,61 +73,3 @@ export class WarriorForm extends Component {
 }
 
 export default WarriorForm;
-
-// function WarriorFormPanel() {
-//   return (
-//     <div className="WarriorFormDiv">
-//       <h3>form of warriors</h3>
-
-//       <div className="container">
-//         <form>
-//           <div className="form-gruop">
-//             <label htmlFor="faridy-form-1">first name</label>
-//             <input
-//               type="text"
-//               className="form-control"
-//               id="formInput1"
-//               placeholder="Example input"
-//             />
-//           </div>
-//           <div className="form-gruop">
-//             <label htmlFor="faridy-form-2">last name</label>
-//             <input
-//               type="text"
-//               className="form-control"
-//               id="formInput2"
-//               placeholder="Example input"
-//             />
-//           </div>
-//           <div className="form-gruop">
-//             <label htmlFor="faridy-form-3">serverice id</label>
-//             <input
-//               type="text"
-//               className="form-control"
-//               id="formInput3"
-//               placeholder="Example input"
-//             />
-//           </div>
-//           <div className="form-group">
-//             <label htmlFor="faridy-form-3">branch of service</label>
-//             <select name="" id="" className="form-control"></select>
-//           </div>
-//           {/* {this.props.map} */}
-
-//           <table className="table">
-//             <thead className="thead-dark">
-//               <tr>
-//                 <th>Fname</th>
-//                 <th>Lname</th>
-//                 <th>serviceId</th>
-//                 <th>branch</th>
-//               </tr>
-//             </thead>
-//           </table>
-//         </form>
-//       </div>
-//     </div> //it seems only one div should be wrapping whole, here
-//   );
-// }
-
-// export default WarriorFormPanel;
