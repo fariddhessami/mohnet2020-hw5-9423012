30ad669925a633ec92ba8e1bd486343036c2abdd
fariddhessami <fariddhessami@gmail.com>
2020-05-24 13:21:49 +0430

    so I've adde bootstrap-react package by : npm install react-bootstrap bootstrap

---
+++ client-react-app/src/components/WarriorForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './App.css';
+
+function WarriorFormPanel() {
+  return (
+    <div className="WarriorFormDiv">
+      <h3>form discriptors</h3>
+    </div>
+  );
+}
+
+export default WarriorFormPanel;
