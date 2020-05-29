0f00c1fdd29199a4efbbb8cace8a3d78c68abc30
fariddhessami <fariddhessami@gmail.com>
2020-05-29 16:28:48 +0430

    back end server now sends a test jason stuff

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ const expressServer = express();
 
 const PORT = 5000;
 
-expressServer.use(require('./routes/routes.js'));
+expressServer.use(require('./src/routes/routes.js'));
+
+var forDes_Data = require('./src/dataFiles/testData.json');
+// console.log(forDes_Data);
+
+router.get('/api/forms-test', (req, res) => {
+    res.send(forDes_Data);
+    console.log('server recieved a test-data request');
+});
 
 expressServer.listen(PORT, () =>
     console.log(`express server is now listening on port ${PORT}`)
