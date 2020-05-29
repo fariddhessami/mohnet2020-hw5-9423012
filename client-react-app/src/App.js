007f7e2833b911622e7437fe936b2ae5e39a0ff8
fariddhessami <fariddhessami@gmail.com>
2020-05-29 14:07:48 +0430

    first step of istalling material UI

--- client-react-app/src/App.js
+++ client-react-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 // import FormDiscPanel from './components/FormDiscriptorsPanel';
 // import WarriorFormPanel from './components/WarriorForm';
--- client-react-app/src/components/WarriorForm.js
+++ client-react-app/src/components/WarriorForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 
